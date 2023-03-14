d854dd5428a14b407d163052d91b82ba9627b0e3
Hamza <118851400+MehdiKaturipo@users.noreply.github.com>
2023-03-14 17:24:54 +0100

    h

--- code.c
+++ code.c
@@ -2,6 +2,7 @@
 void laouinayassine() {printf("konfli");}
 void afficherImad(){printf("imad the boss");}
 void mouad() {printf("mouad");}
+void MehdiS() {printf("Mehdi");}
 main(){
 	printf("principale");
 }
