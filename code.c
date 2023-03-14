b38bea2c327c93978cd8756259a2ce98aca66d2b
anissakheirallah <117165100+anissakheirallah@users.noreply.github.com>
2023-03-14 17:27:11 +0100

    Update code.c

--- code.c
+++ code.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
-void laouinayassine() {printf("konfli");}
-void afficherImad(){printf("imad the boss");}
+void afficherAnissa(){printf("anissa");}
+}
 main(){
 	printf("principale");
+
 }
