6934929a12768ed48a5dafd34bc0560c7ad9092f
Lalykhalfaoui <106170564+Lalykhalfaoui@users.noreply.github.com>
2023-03-14 17:27:07 +0100

    Update code.c

--- code.c
+++ code.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 void laouinayassine() {printf("konfli");}
 void afficherImad(){printf("imad the boss");}
+void afficherLaila(){printf("laila khalfaoui");}
 main(){
 	printf("principale");
 }
